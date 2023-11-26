4440297b35b51601c25142b33780472ec444df77
Justin Gilman <eyeofmidas@gmail.com>
2023-11-26 06:56:18 -0800

    getting drawer to start hidden and then display

--- editor/editor.js
+++ editor/editor.js
@@ -1,4 +1,10 @@
 import DomBuilder from "./library/dombuilder.js"
 document.addEventListener("DOMContentLoaded", () => {
-    new DomBuilder().addComponent("drawer", document.body)
+    new DomBuilder().addComponent("drawer", document.body).then((drawer) => {
+        setTimeout(() => {
+            drawer.style.display = "block"
+            drawer.style.visibility = "visible"
+            console.log("enabling drawer")
+        }, 3000)
+    })
 })
